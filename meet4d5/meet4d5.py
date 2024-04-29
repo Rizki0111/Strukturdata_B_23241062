05226d19a6b2f2570ea9f247f6fe49920e19bce5
Rizki0111 <146050947+Rizki0111@users.noreply.github.com>
2024-04-29 11:57:48 +0800

    senin 29-04-2024

---
+++ meet4d5/meet4d5.py
@@ -0,0 +1,50 @@
+# inputan dari user
+# aritmatika
+
+# belajar inputan 
+#Rizki = input("masukkan kata :")
+# print("isi dari rizki :",Rizki,"bertipe data :", type(Rizki))
+
+# belajar aritmatika dasar
+x = 11
+y = 4
+
+# penjumlahan +
+hasil = x + y
+print ("x + y =", hasil)
+
+# pengurangan 
+hasil = x - y
+print ("x - y =", hasil)
+
+# perkalian 
+hasil = x * y
+print ("x * y =", hasil)
+
+# pembagian 
+hasil = x / y
+print ("x / y =", hasil)
+
+# perpangkatan exponen
+hasil = x ** y
+print ("x ^ y =", hasil)
+
+# modulus %
+hasil = y % x
+print ("x mod y =", hasil)
+
+# floor division (pembulatan kebawah) //
+hasil = x // y
+print ("x // y =", hasil)
+
+# aritmatika prioritas
+# (), exponen, perkalian/pembagian, penjumlahan/pengurangan 
+x = 3
+y = 4 
+z = 5
+
+hasil = ( x ** y *  (z + x) / y - z % x // y)
+print(hasil)
+
+
+
