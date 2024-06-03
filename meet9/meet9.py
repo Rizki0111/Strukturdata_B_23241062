4077dacfd6ae51123e116711fd6e295d4be4e435
Rizki0111 <146050947+Rizki0111@users.noreply.github.com>
2024-06-03 11:47:42 +0800

    Add files via upload

---
+++ meet9/meet9.py
@@ -0,0 +1,22 @@
+# percabangan jika maka
+
+name = input("what is your name : ")
+
+#if kondisi : aksi
+
+if name == "edy" : print("dosen ganteng")
+print("end of program")
+
+# 2. if identitas
+#if name=="edy":
+#   print("dosen ganteng")
+#   print("ganteng banget")
+#print("end of program")
+
+# 3. IF ELSE
+if name=="edy":
+    print("dosen ganteng")
+    print("terima kasih")
+else:
+    print("anda tidak ganteng")
+print("end of program") 
